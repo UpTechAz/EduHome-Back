537999425cfb150383213575ce2a4cb78dd98a74
Parvina24 <ismayilovaparvina23@gmail.com>
2024-05-06 13:43:22 +0400

    Events

--- ViewModels/Home/HomeIndexVM.cs
+++ ViewModels/Home/HomeIndexVM.cs
@@ -6,7 +6,7 @@ namespace WebApplication2.ViewModels.Home
     {
         public List<Slider>? Sliders { get; set; }
         public List<Blog>? Blogs { get; set; }
-        public List<Event>? Events { get; set; }
+        public List<Models.Event>? Events { get; set; }
         public List<NoticeBoard>? NoticeBoards { get; set; }
         public List<Course>? Courses { get; set; }
         public EducationTheme? EducationTheme { get; set; }
